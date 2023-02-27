bfd2a7434c5c85c1198a9b12055f4b98bbac5a36
seacrest3 <o7716442@gmail.com>
2023-02-27 09:42:04 +0000

    added code for License section in generateMarkdown.js file

--- output/README.md
+++ output/README.md
@@ -1,2 +1,5 @@
-# PPP
+# SSS
+  [![License: MIT](https://img.shields.io/badge/license-MIT-yellow.svg)
+  ## License
+  This application is covered by the [MIT](https://opensource.org/licenses/MIT) license.
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,19 +11,33 @@ function generateMarkdown(data) {
     email } = data;
 
     let badge = ``;
+    let licenseLink = ``;
 
-  if (license == 'MIT')
-    badge = `[![License: MIT](https://img.shields.io/badge/license-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-  if (license == 'ISC')
-    badge = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
-  if (license == 'Boost 1.0')
-    badge = `[![License: Boost 1.0](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
-  if (license == 'GNU GPL v3')
-    badge = `[![License: GNU GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-  if (license == 'BSD 3')
-    badge = `[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  if (license == 'MIT') {
+    badge = `[![License: MIT](https://img.shields.io/badge/license-MIT-yellow.svg)`;
+    licenseLink = `https://opensource.org/licenses/MIT`;
+  }
+  if (license == 'ISC') {
+    badge = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]`;
+    licenseLink = `https://opensource.org/licenses/ISC`;
+  }
+  if (license == 'Boost 1.0') {
+    badge = `[![License: Boost 1.0](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]`;
+    licenseLink = `https://www.boost.org/LICENSE_1_0.txt`;
+  }
+  if (license == 'GNU GPL v3') {
+    badge = `[![License: GNU GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
+    licenseLink = `https://www.gnu.org/licenses/gpl-3.0`;
+  }
+  if (license == 'BSD 3') {
+    badge = `[![License: BSD 3](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`;
+    licenseLink = `https://opensource.org/licenses/BSD-3-Clause`;
+  }
   return `# ${title}
   ${badge}
+  ## License
+  This application is covered by the [${license}](${licenseLink}) license.
+  
 `;
 }
 
